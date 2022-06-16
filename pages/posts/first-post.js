7bf593d70e5279ff478f6a996c6a0e1b33cdf8c5
Alain Horner <alain.hornair@gmail.com>
2022-06-16 13:20:30 -0500

    custom layout component with custom css module

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "/components/layout";
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>Le title</title>
+            </Head>
             <h1>This is my first post</h1>
             <h2>
                 <Link href={'/'}>Back to home</Link>
             </h2>
-        </>
+        </Layout>
     )
 }
