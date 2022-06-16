3c917cb3596a2836ca08d5227157961aecd8601e
Alain Horner <alain.hornair@gmail.com>
2022-06-16 15:34:55 -0500

    render markdown content, format date, link statically generated pages on home

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
 import Layout from "/components/layout";
+import Head from "next/head";
+import Date from "/components/date";
 import {getAllPostIds, getPostData} from "/lib/posts";
+import utilStyles from '/styles/utils.module.css';
 
 export async function getStaticPaths() {
     return {
@@ -11,7 +14,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
     return {
         props: {
-            postData: getPostData(params.id),
+            postData: await getPostData(params.id),
         },
     };
 }
@@ -19,11 +22,16 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
     return (
         <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     )
 }
