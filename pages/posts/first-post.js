aed3f90b15ff28e47810afff678d94b6878c9eeb
Alain Horner <alain.hornair@gmail.com>
2022-06-16 13:49:43 -0500

    tailwind and postcss setup

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -8,7 +8,7 @@ export default function FirstPost() {
             <Head>
                 <title>Le title</title>
             </Head>
-            <h1>This is my first post</h1>
+            <h1 className="block text-red-600">This is my first post</h1>
         </Layout>
     )
 }
