662b4b4cc571ad05981f77755d64310f41db9a47
Alain Horner <alain.hornair@gmail.com>
2022-06-17 16:08:54 -0500

    fix indentation rule if I can't use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-  extends: [
-    'standard',
-    'next'
+    extends: [
+        'standard',
+        'next'
     // "prettier"// TODO: when this is activated, eslint doesn't autofix anymore - But I would love to have this :)
-  ],
-  rules: {
-    semi: [1, 'always']
-  }
+    ],
+    rules: {
+        semi: [1, 'always'],
+        indent: ['error', 4]
+    }
 };
