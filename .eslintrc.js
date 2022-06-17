bafcbb09421fb3e8536fefa106f4ab31c42891b9
Alain Horner <alain.hornair@gmail.com>
2022-06-17 16:06:50 -0500

    better eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'standard',
+    'next'
+    // "prettier"// TODO: when this is activated, eslint doesn't autofix anymore - But I would love to have this :)
+  ],
+  rules: {
+    semi: [1, 'always']
+  }
+};
