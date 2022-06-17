1a018effd870cabe41b733941ff7d7263ffc9176
Alain Horner <alain.hornair@gmail.com>
2022-06-17 16:16:59 -0500

    linting and autofixing

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,5 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function hello(_: NextApiRequest, res: NextApiResponse)    {
-    res.status(200).json({ text: 'Hello' })
-
-    if ( 0 == 0) {
-        return 124;
-    }
+export default function hello (_: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json({ text: 'Hello' });
 }
