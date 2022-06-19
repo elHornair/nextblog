9eda03776c7d182597aabafcbbbbdda7c46ab189
Alain Horner <alain.hornair@gmail.com>
2022-06-18 21:34:35 -0500

    extract HeroSection to its own component

---
+++ components/HeroSection.tsx
@@ -0,0 +1,40 @@
+import utilStyles from '../styles/utils.module.css';
+import Image from 'next/image';
+import React from 'react';
+
+export default function HeroSection () {
+    return <section className="relative bg-gray-800">
+        <div className={utilStyles.contentContainer}>
+            <div className="pt-1 pb-10 px-4 sm:px-6 lg:w-1/2 lg:mx-0 lg:pt-20 lg:px-8 lg:pr-20 xl:pt-28">
+                <div className="sm:text-center lg:text-left">
+                    <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl xl:text-6xl">
+                        <span className="sm:block">Instead, let&apos;s go</span>{' '}
+                        <span className="text-orange-400 sm:block">cycling</span>
+                    </h1>
+                    <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                        This blog is all about bikepacking, bicycle travelling and all the <span
+                            className="line-through">pain</span> fun that comes with it.
+                    </p>
+                </div>
+            </div>
+            <div className="relative h-36 lg:h-auto lg:absolute lg:inset-y-0 lg:right-0 lg:-mt-24 lg:w-1/2">
+                <svg
+                    className="hidden lg:block absolute z-10 left-0 inset-y-0 h-full w-48 text-gray-800 transform -translate-x-1/2"
+                    fill="currentColor"
+                    viewBox="0 0 100 100"
+                    preserveAspectRatio="none"
+                    aria-hidden="true"
+                >
+                    <polygon points="50,0 100,0 50,100 0,100"/>
+                </svg>
+                <Image
+                    priority
+                    className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+                    src="/images/hero.jpg"
+                    layout="fill"
+                    alt="Bikepacking bicycle in nature."
+                />
+            </div>
+        </div>
+    </section>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,9 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import Date from '../components/date';
 import Link from 'next/link';
-import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
-import Image from 'next/image';
 import React from 'react';
+import HeroSection from '../components/HeroSection';
 
 export async function getStaticProps () {
     return {
@@ -21,39 +20,8 @@ export default function Home ({ allPostsData }) {
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            <section className="relative bg-gray-800">
-                <div className={utilStyles.contentContainer}>
-                    <div className="pt-1 pb-10 px-4 sm:px-6 lg:w-1/2 lg:mx-0 lg:pt-20 lg:px-8 lg:pr-20 xl:pt-28">
-                        <div className="sm:text-center lg:text-left">
-                            <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl xl:text-6xl">
-                                <span className="sm:block">Instead, let&apos;s go</span>{' '}
-                                <span className="text-orange-400 sm:block">cycling</span>
-                            </h1>
-                            <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                            This blog is all about bikepacking, bicycle travelling and all the <span className="line-through">pain</span> fun that comes with it.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="relative h-36 lg:h-auto lg:absolute lg:inset-y-0 lg:right-0 lg:-mt-24 lg:w-1/2">
-                        <svg
-                            className="hidden lg:block absolute z-10 left-0 inset-y-0 h-full w-48 text-gray-800 transform -translate-x-1/2"
-                            fill="currentColor"
-                            viewBox="0 0 100 100"
-                            preserveAspectRatio="none"
-                            aria-hidden="true"
-                        >
-                            <polygon points="50,0 100,0 50,100 0,100" />
-                        </svg>
-                        <Image
-                            priority
-                            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-                            src="/images/hero.jpg"
-                            layout="fill"
-                            alt='Bikepacking bicycle in nature.'
-                        />
-                    </div>
-                </div>
-            </section>
+
+            <HeroSection/>
 
             <section>
                 <h2>From the blog</h2>
@@ -63,9 +31,9 @@ export default function Home ({ allPostsData }) {
                             <Link href={`/posts/${id}`}>
                                 <a>{title}</a>
                             </Link>
-                            <br />
+                            <br/>
                             <small>
-                                <Date dateString={date} />
+                                <Date dateString={date}/>
                             </small>
                         </li>
                     ))}
