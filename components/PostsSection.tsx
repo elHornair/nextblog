881feaedd1b2cb7c653c26b02c48038cb2958176
Alain Horner <alain.hornair@gmail.com>
2022-06-19 19:37:14 -0500

    text

--- components/PostsSection.tsx
+++ components/PostsSection.tsx
@@ -11,10 +11,10 @@ export default function PostsSection ({ allPosts }) {
             <div className={utilStyles.contentContainer}>
                 <div className="relative pt-16 pb-20 lg:pt-24 lg:pb-28">
                     <div className="text-center">
-                        <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">From the blog</h2>
+                        <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Latest articles</h2>
                         <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-                        You&apos;re probably busy riding. But in case you&apos;re looking for inspiration,
-                        check out these blog posts:
+                            You&apos;re probably busy riding. But in case you&apos;re looking for inspiration,
+                            check out the latest blog posts:
                         </p>
                     </div>
                     <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
